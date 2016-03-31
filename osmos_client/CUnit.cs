27bee5d827d97a529f2a31c524ce3390a74e956b
Егор Шимко < >
2016-03-31 17:40:16 +0300

    init and base class

---
+++ osmos_client/CUnit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osmos_client
+{
+    class CUnit
+    {
+        protected int x, y;//позиция
+        protected double scale;//размер
+        protected int radX, radY;//координаты круга 
+        protected int radUnit;
+
+        public void cheackCollide(CUnit unit)
+        {
+            double d = Math.Sqrt(Math.Pow((this.x + ((this.scale ) * this.radX)) -
+                    (unit.x + ((unit.scale ) * unit.radX)), 2) +
+                    Math.Pow((this.y + (this.scale * this.radY)) -
+                    (unit.y + (unit.scale * radY)), 2));
+
+            if (d < (this.scale * this.radUnit + unit.scale * unit.radUnit))
+            {
+                if (this.scale > unit.scale)
+                {
+                    //пересчет скейла + зависит от скорости/инерции
+                }
+                else
+                {
+                    //пересчет скейла - зависит от скорости/инерции
+                }
+
+                return;
+            }
+        }
+
+        public void draw()
+        {
+
+            //отрисовка
+        }
+            
+        
+
+
+    }
+}
